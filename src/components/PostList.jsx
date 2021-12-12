ada73bd64da6c53a49ca49bc358a354cb524eb31
Slay <destpoch@mail.ru>
2021-12-12 11:03:57 +0300

    before routing

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import PostItem from './PostItem';
+import {
+    CSSTransition,
+    TransitionGroup,
+  } from 'react-transition-group';
+
 
 const PostList = ({posts, title, remove}) => {
 
@@ -17,12 +22,26 @@ const PostList = ({posts, title, remove}) => {
         <h1 style={{textAlign:'center'}}>
             {title}
             </h1>
+
+            <TransitionGroup>
+
             {posts.map((post, index) =>
-          <PostItem remove={remove} number={index+1} post={post} key={post.id}/>
+
+                <CSSTransition
+                key={post.id}
+                timeout={500}
+                classNames="post"
+                >
+                    <PostItem remove={remove} number={index+1} post={post} />
+                </CSSTransition>
+
+
         )}
 
+
+            </TransitionGroup>
+
       
- 
         </div>
     );
 };
---
+++ src/pages/About.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route
+  } from "react-router-dom";
+
+const About = () => {
+    return (
+
+
+            <Route>
+            <h1>
+
+                application works
+
+            </h1>
+            </Route>
+
+
+    );
+};
+
+export default About;
